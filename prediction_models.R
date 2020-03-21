b11e87956d76fee3868adf65dc86d5e18f631820
Taopi <david.schwelien@srgssr.ch>
2020-03-21 11:18:38 +0100

    data sets

--- prediction_models.R
+++ prediction_models.R
@@ -1,5 +1,5 @@
 ## packages
-list.of.packages <- c("readr", "forecast", "pracma","Metrics","readr","dplyr","tibble","reshape")
+list.of.packages <- c("readr", "forecast", "pracma","Metrics","readr","dplyr","tibble","reshape","zoo")
 new.packages <- list.of.packages[!(list.of.packages %in% installed.packages()[,"Package"])]
 if(length(new.packages)) install.packages(new.packages)
 
@@ -10,21 +10,42 @@ library (readr)
 #international data
 path <- "/Users/davidschwelien/Desktop/Modell_Infos/ts_r.csv" ### issue: read from git!
 covid_world_data <- read_csv(path)
-covid_world_data  <- as.data.frame(covid_world_data )
+covid_world_data  <- as.data.frame(covid_world_data)
 
 #swiss data:
-urlfile <- "https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/predicted.csv"
+urlfile <- "https://raw.githubusercontent.com/daenuprobst/covid19-cases-switzerland/master/covid19_cases_switzerland.csv"
 covid_swiss_data <- read.csv(url(urlfile))
 covid_swiss_data[,1] <- as.Date(covid_swiss_data[,1])
-c("Switzerland_",names(covid_swiss_data))
 
-# merge and clean
+# merge datasets
 names(covid_swiss_data) <- paste("Switzerland_", names(covid_swiss_data), sep='')
 names(covid_swiss_data)[1] <- "date"
 library(dplyr)
-covid_all_data  <- left_join(covid_world_data, covid_swiss_data)
-covid_all_data[is.na(covid_all_data)] <- 0
-#swiss_compare<-covid_all_data[,grepl( "wit" , names( covid_all_data ) )]
+covid19_cases  <- left_join(covid_world_data, covid_swiss_data)
+covid19_cases[is.na(covid19_cases)] <- 0
+
+# beautify data sets
+colnames(covid19_cases) <- gsub("nan", "all", colnames(covid19_cases))
+colnum <-which(colnames(covid19_cases) =="Switzerland_CH")
+colnames(covid19_cases)[colnum] <- paste("Switzerland_all_scraped")
+
+# subset Switzerland
+x <- c("date", "wit")
+# oder COLs
+covid19_cases_switzerland <- covid19_cases[,grepl( paste(x, collapse = "|") , names(covid19_cases))]
+col_names <- names(covid19_cases_switzerland)
+first_cols  <- c("date", "Switzerland_all", "Switzerland_all_scraped")
+'%ni%' <- Negate('%in%')
+last_cols <- col_names[col_names%ni%first_cols]
+col_order <- c(first_cols, last_cols)
+covid19_cases_switzerland <-covid19_cases_switzerland[,col_order]
+
+#write csv
+write.csv(covid19_cases_switzerland, "covid19_cases_switzerland.csv")
+write.csv(covid19_cases, "covid19_cases.csv")
+
+## clean data sets:
+covid19_cases <- na.locf(covid19_cases, fromLast = TRUE)
 
 ### predict data
 
@@ -34,8 +55,7 @@ library(Metrics)
 library(readr)
 library(tibble)
 
-all <- covid_all_data
-### wip:
+all <- covid19_cases
 all <- as_tibble(all) 
 ###
 all$X1<-NULL
@@ -99,25 +119,27 @@ names(pred_all)[5]<-"X2"
 pred_all=pred_all[with( pred_all, order(region, date)), ]
 
 ### clean data; export data
-covid_all_data_predicted <-  pred_all[,c(1,2,4)]
-covid_all_data_predicted[,3] <- round(covid_all_data_predicted[,3])
+covid19_cases_predicted <-  pred_all[,c(1,2,4)]
+covid19_cases_predicted[,3] <- round(covid19_cases_predicted[,3])
 library(reshape)
-covid19_cases_predicted <- cast(covid_all_data_predicted, date ~ region)
-colnames(covid19_cases_predicted) <- gsub("nan", "all", colnames(covid19_cases_predicted))
+covid19_cases_predicted <- cast(covid19_cases_predicted, date ~ region)
+#colnames(covid19_cases_predicted) <- gsub("nan", "all", colnames(covid19_cases_predicted))
 ### 
-colnum <-which(colnames(covid19_cases_predicted) =="Switzerland_all")
-colnames(covid19_cases_predicted)[colnum] <- paste("Switzerland_all")
-colnum <-which(colnames(covid19_cases_predicted) =="Switzerland_CH")
-colnames(covid19_cases_predicted)[colnum] <- paste("Switzerland_all_scraped")
+#colnum <-which(colnames(covid19_cases_predicted) =="Switzerland_all")
+#colnames(covid19_cases_predicted)[colnum] <- paste("Switzerland_all")
+#colnum <-which(colnames(covid19_cases_predicted) =="Switzerland_CH")
+#colnames(covid19_cases_predicted)[colnum] <- paste("Switzerland_all_scraped")
 write.csv(covid19_cases_predicted,"covid19_cases_predicted.csv")
+
 # subset Switzerland
 x <- c("date", "wit")
+# oder COLs
 covid19_cases_predicted_switzerland <- covid19_cases_predicted[,grepl( paste(x, collapse = "|") , names(covid19_cases_predicted))]
-
 col_names <- names(covid19_cases_predicted_switzerland)
 first_cols  <- c("date", "Switzerland_all", "Switzerland_all_scraped")
 '%ni%' <- Negate('%in%')
 last_cols <- col_names[col_names%ni%first_cols]
 col_order <- c(first_cols, last_cols)
 covid19_cases_predicted_switzerland <-covid19_cases_predicted_switzerland[,col_order]
+
 write.csv(covid19_cases_predicted_switzerland, "covid19_cases_predicted_switzerland.csv")
